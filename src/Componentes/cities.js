852e30a31cc2b43bf738dbbb99bdb21bcf1cc69a
emaxisalas@gmail.com <94768370+Maxirx@users.noreply.github.com>
2022-02-22 16:22:51 -0300

    sd

--- src/Componentes/cities.js
+++ src/Componentes/cities.js
@@ -1,11 +1,15 @@
 const Ciudades = () => {
+
+
     return (
         <main id="principal2">
             <h1>
                 Under Construction
             </h1>
             <h2>Find your perfect trip, designed by insiders who know and love their cities!</h2>
-
+            <form action=""><label for="text">Buscar</label>
+                <input type="text" id="text"></input>
+            </form>
         </main>
     )
 
