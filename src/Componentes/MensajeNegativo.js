86e0c6f738070fdd5c5cfe80c74f6b8fb8c0614f
emaxisalas@gmail.com <94768370+Maxirx@users.noreply.github.com>
2022-03-10 09:25:38 -0300

    links

--- src/Componentes/MensajeNegativo.js
+++ src/Componentes/MensajeNegativo.js
@@ -12,8 +12,8 @@ const MensajeNegativo = () => {
             <img src={'https://static.vecteezy.com/system/resources/previews/004/595/728/non_2x/backpacker-guy-standing-front-road-sign-board-in-street-confusing-choose-way-tourist-travel-guide-symbol-set-concept-in-cartoon-illustration-on-white-background-vector.jpg'} alt="bags lost" className="imagenPerdida" />
             <Stack spacing={5} direction="row">
 
-                <LinkRouter to={"/cities"}><Button variant="contained">Go to Back</Button>
-                    <LinkRouter to={"/"}></LinkRouter><Button variant="contained">Go to Home</Button></LinkRouter>
+                <LinkRouter to={"/cities"}><Button variant="contained">Go to Back</Button></LinkRouter>
+                <LinkRouter to={"/volva"}><Button variant="contained">Go to Home</Button></LinkRouter>
             </Stack>
         </div>
     )
